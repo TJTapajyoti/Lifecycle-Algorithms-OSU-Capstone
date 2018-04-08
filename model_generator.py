d8e113cbfc517be974fe3192c75c7193af0db2f5
Matthias Heinz <heinz.matthias.3@gmail.com>
2018-04-08 19:24:46 -0400

    Add model generator stub

---
+++ model_generator.py
@@ -0,0 +1,35 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+from __future__ import absolute_import
+import numpy as np
+
+
+class Final_Model_Generator(object):
+
+    def __init__(self):
+        pass
+
+    def add_process(self, code, value, env_impact, uncertainty):
+        pass
+
+    def add_process_input(self, output_process_code, input_code, value):
+        pass
+
+    def has_process(self, code):
+        boolean = False
+        return boolean
+
+    def has_process_input(self, output_process_code, input_code):
+        boolean = False
+        return boolean
+
+    def create_new_matrix_and_calculate(self):
+        boolean = False
+        return boolean
+
+    def clear_unfinalized_data(self):
+        pass
+
+    def finalize(self):
+        pass
